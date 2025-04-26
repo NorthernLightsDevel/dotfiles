7cbd740b11e5f557a06c8f318fafb02eb3605e67
Per Øyvind Haavelsrud <peroyvind@havelsrud.com>
2025-04-26 19:54:38 +0200

    Set up more configs.

--- update-dotnet.sh
+++ update-dotnet.sh
@@ -9,6 +9,7 @@ fi
 # Install/update LTS runtimes currently in use.
 sudo ${HOME}/dotnet-install.sh -c 6.0 --install-dir /usr/share/dotnet/
 sudo ${HOME}/dotnet-install.sh -c 8.0 --install-dir /usr/share/dotnet/
+sudo ${HOME}/dotnet-install.sh -c 9.0 --install-dir /usr/share/dotnet/
 
 # link dotnet executable into /usr/bin/dotnet to ensure command is executable.
 sudo ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet 2>/dev/null
